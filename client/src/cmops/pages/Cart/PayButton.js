dabbaf1b931970c4e12eb499b54053447f4fc7dc
David Tassama <davidtassama464@gmail.com>
2022-11-14 20:58:50 +0200

    updata

--- client/src/cmops/pages/Cart/PayButton.js
+++ client/src/cmops/pages/Cart/PayButton.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import axios from "axios";
 import Button from "react-bootstrap/Button";
@@ -22,28 +21,3 @@ export default function PayButton({ cartItem }) {
     </div>
   );
 }
-=======
-import React from "react";
-import axios from "axios";
-import Button from "react-bootstrap/Button";
-
-export default function PayButton({ cartItem }) {
-  const handleCheckout = () => {
-    axios
-      .post(`https://dji-store.netlify.app/stripe/create-checkout-session`, {
-        cartItem,
-      })
-      .then((res) => {
-        if (res.data.url) {
-          window.location.href = res.data.url;
-        }
-      })
-      .catch((err) => console.log(err.massage));
-  };
-  return (
-    <div>
-      <Button onClick={() => handleCheckout()} variant="outline-secondary">CHECKOUT SECURELY NOW</Button>
-    </div>
-  );
-}
->>>>>>> a58dec3e953c3cda4e322e3affa51a3b6375819d
