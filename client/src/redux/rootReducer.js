5f6a9577b5795dc7b3e934fbd666b34db8b7893d
David Taesema <105584546+DavidTesema@users.noreply.github.com>
2022-11-26 13:10:42 +0200

    Delete client directory

--- client/src/redux/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import shopReducer from "./shopping/ShoppingReducer"
-
-const rootReducer = combineReducers({
-    shop : shopReducer
-});
-
-export default rootReducer;
- 
--- client/src/redux/shopping/ShoppingTypes.js
+++
@@ -1,5 +0,0 @@
-export const ADD_TO_CART = "ADD_TO_CART";
-export const ADD_ALL_PRODUCT = "ADD_ALL_PRODUCT";
-export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
-export const ADJUST_QTY = "ADJUST_QTY";
-export const LOAD_CURRENT_ITEM = "LOAD_CURRENT_ITEM";
